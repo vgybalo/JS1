a5b936667d9a934e33129c5be14c13287eed3dea
vgybalo <vgybalo@ukr.net>
2019-05-20 22:25:11 +0300

    second commit

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-
-let $login = 'admin';
-let $login_false = typeof $login;
-let $not_object_notif = 'Login have not be object or null';
-let $not_number_notif = 'Login have not be number';
-let $not_string_notif = 'Login have include letters';
-let $input_notif = 'Please input login';
-let $more_symbol = 'Please use more than 3 symbols in your login';
-
-
-if ($login_false == 'undefined') {
-    console.log ($input_notif);
+/*let b = 1;
+let c = +prompt("Введите число для расчета факториала");
+for (let i = 1; i<=c; i++) {
+    b = b * i;
 }
-else if ($login_false == 'number'){
-    console.log ($not_number_notif);
-}
-else if ($login_false == 'object'){
-    console.log ($not_object_notif);
-}
-else if ($login_false != 'string'){
-    console.log ($not_string_notif);
-}
-else if ($login < 'aaa'){
-    console.log ($more_symbol);
-}
-else {
-    console.log('Your login is ' + $login);
-    }
+console.log(b);*/
 
-
-/*Array*/
-let typeof_arr = [ {string:'Петя'}, {number:2}, {boolean:true}, {object:'object'}, {NaN:'NaN'}, {undefined:'undefined'}, {infinity:'infinity'}, {infinity:'null'}];
-console.log(typeof_arr[4]);
+let b = 1;
+let c = +prompt("Введите число для расчета факториала");
+let i = 1;
+while (i<=c) {
+    b = b * i;
+    i++;
+}
+console.log(b);
