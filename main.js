a71260ec96a03121d8af50315539081ba59877a2
vgybalo <vgybalo@ukr.net>
2019-05-23 22:47:47 +0300

    fourth commit

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-let b = +prompt("Введите стартовое число суммы арифметической прогрессии");;
-let c = +prompt("Введите число на которое будет увеличиваться прогрессия");
-let d = +prompt("Введите количество шагов арифметической прогрессии");
-let e = 0;
-for (let i = 0; i<d; i++) {
-    e = b + c*i;
-    console.log(e);
+
+let arr = [1, 2, 'apple', 'orange', 3, 4, ['pine', 'eve', 'rose'], 5, 6, ['red', 'green', 'blue'], true, 7, 8, 'stone'];
+let string = [];
+let number = [];
+for (let i = 0; i<arr.length; i++) {
+    let element = arr[i];
+    if (typeof element == 'string') {
+        string.push(element);
+    }
+    else if (typeof element == 'number') {
+        number.push(element);
+    }
+    else if (typeof element == 'object') {
+        for (let j = 0; j<element.length; j++) {
+            let inner_element = element[j];
+            if (typeof inner_element == 'string') {
+                string.push(inner_element);
+            }
+            else if (typeof inner_element == 'number') {
+                number.push(inner_element);
+            }
+        }
+    }
 }
 
+for (let k = 0; k<string.length; k++) {
+    console.log(string[k]);
+}
 
+for (let l = 0; l<number.length; l++) {
+    console.log(number[l]);
+}
