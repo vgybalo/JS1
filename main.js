89bf41c7d98245a777ababdcfc215849a8784a7a
vgybalo <vgybalo@ukr.net>
2019-06-17 21:50:27 +0300

    sixteenth commit

--- main.js
+++ main.js
@@ -1,35 +1,16 @@
-let input = document.querySelector('input');
-let addValue = document.querySelector('#addbutton');
-let cleanValue = document.querySelector('#cleanbutton');
-let calculate = document.querySelector('#calculate');
-let output = document.querySelector('#output');
-let inputValues = [];
-let c = 0;
-
-addValue.addEventListener('click', function () {
-        inputValues.push(input.value);
-        console.log(inputValues);
-    }
-);
-
-cleanValue.addEventListener('click', function () {
-        inputValues.pop();
-        console.log(inputValues);
-    }
-);
-
-calculate.addEventListener('click', calc );
-calculate.addEventListener('click', indicate );
-
-function calc () {
-    let b= 1;
-    for (let i = 0; i < inputValues.length; i++) {
-        b *= Number(inputValues[i]);
-    }
-    c = b/ inputValues.length;
-    console.log(c);
+let a = 0;
+function display(callback) {
+    console.log(callback());
 }
 
-function indicate () {
-    output.value = c;
+
+
+
+setTimeout( transfer , 1000);
+
+function transfer() {
+    a = 134;
+    return a;
 }
+
+display (transfer);
