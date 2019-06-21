67c58bd401f8c96c6392447039b19b15e694b216
vgybalo <vgybalo@ukr.net>
2019-06-21 18:52:53 +0300

    eighteenth commit

--- main.js
+++ main.js
@@ -1,16 +1,60 @@
-let a = 0;
-function display(callback) {
-    console.log(callback());
-}
+
+let addbutton = document.querySelector('#addbutton');
+let input1 = document.querySelector('#input1');
+let block1 = document.querySelector('.block1');
+let main_form = document.querySelector('.main_form');
+/*let block2 = document.querySelector('.block2');
+let h1_2 = document.querySelector('.block2_h1');*/
+let i = 1;
+
+addbutton.addEventListener('click', function () {
+    let div = document.createElement('div');
+    let input2 = document.createElement('input');
+    let div2 = document.createElement('div');
+
+    //let inner_div1 = document.querySelector('.block2');
+
+
+    div.innerHTML = input1.value;
+    input2.type = 'button';
+    input2.value = '-';
+
+    block1.appendChild(div);
+    div.appendChild(input2);
+    div.appendChild(div2);
+   /*e.target.addEventListener('click', function () {
+        div2.innerHTML = i;
+        i++;
+    });*/
+    for (let i = 0; i < main_form.childNodes.length; i++) {
+        //console.log(main_form.childNodes[i])
+        console.log(main_form.childNodes.index);
+
+    }
+
+
+        input2.addEventListener('click', function() {
+
+        div.remove();
+
+    i++;
+    });
+
+
+    //block1.insertBefore(div, block1.children[0]);
+    div.style.width = '200px';
+    div.style.height = '100px';
+    div.style.backgroundColor = "#fa0";
+
+});
 
 
 
 
-setTimeout( transfer , 1000);
 
-function transfer() {
-    a = 134;
-    return a;
-}
 
-display (transfer);
+
+
+
+
+
