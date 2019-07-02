ffd2f96f7b964ea8b52e36d39be3f90e0b199574
vgybalo <vgybalo@ukr.net>
2019-07-02 18:36:51 +0300

    twenty first commit

--- main.js
+++ main.js
@@ -1,59 +1,87 @@
-let input1 = document.querySelector('#input1');
-let input2 = document.querySelector('#input2');
-let add_oper = document.querySelector('#addbutton');
-let minus_oper = document.querySelector('#minusbutton');
-let mult_oper = document.querySelector('#multbutton');
-let div_oper = document.querySelector('#divbutton');
-let calculate_oper = document.querySelector('#calculate');
-let output = document.querySelector('#output');
+let goods_name = document.querySelector('#goods_name');
+let goods_quantity = document.querySelector('#goods_quantity');
+let price = document.querySelector('#price');
+let addbutton = document.querySelector('#addbutton');
+let goods_name_buy = document.querySelector('#goods_name_buy');
+let goods_quantity_buy = document.querySelector('#goods_quantity_buy');
+let price_buy = document.querySelector('#price_buy');
+let addbutton_buy = document.querySelector('#addbutton_buy');
+let revenue = document.querySelector('#revenue');
+let price2 = null;
+let goods_quantity_buy_data = null;
 
+//store
+let store = [];
+let goods_store = [];
+goods_name.addEventListener('change', function () {
+    let goods_name_data = goods_name.value;
+    goods_store.push(goods_name_data);
+});
 
-let number1 = null;
-let number2 = null;
-let result = null;
+goods_quantity.addEventListener('change', function () {
+    let goods_quantity_data = Number(goods_quantity.value);
+    goods_store.push(goods_quantity_data);
+});
+price.addEventListener('change', function () {
+    let price_data = Number(price.value);
+    goods_store.push(price_data);
+});
+addbutton.addEventListener('click', function () {
+    store.push(goods_store);
 
-input1.addEventListener('change', function () {
-    number1 = Number(input1.value);
-    console.log(number1);
-    });
-input2.addEventListener('change', function () {
-    number2 = Number(input2.value);
-    console.log(number2);
+    console.log(store);
+});
+
+//buying log
+let buying_log = [];
+let buying_goods = [];
+
+goods_name_buy.addEventListener('change', function () {
+    let goods_name_buy_data = goods_name_buy.value;
+    buying_goods.push(goods_name_buy_data);
+    for (let i = 0; i< store.length; i++) {
+        let goods_store_data = store[i];
+
+        for (let j = 0; j< goods_store_data.length; j++) {
+            let goods_store_data_kinds = goods_store_data[j];
+            if (goods_store_data_kinds == goods_name_buy_data) {
+
+                price_buy.value = goods_store_data[2];
+                price2 = goods_store_data[2];
+
+            }
+        }
+    }
+
+});
+
+goods_quantity_buy.addEventListener('change', function () {
+    goods_quantity_buy_data = Number(goods_quantity_buy.value);
+    buying_goods.push(goods_quantity_buy_data);
+});
+
+addbutton_buy.addEventListener('click', function () {
+    buying_log.push(buying_goods);
+
+     let revenue2 = price2 * goods_quantity_buy_data;
+    revenue.value = revenue2;
+
 });
 
 
+/*function shop (goods_name,  goods_quantity, price) {
 
+ buying_goods.push(date);
+    buying_goods.push(cashier);
+    buying_goods.push(goods_name);
+    buying_goods.push(goods_group);
+    buying_goods.push(goods_quantity);
+    buying_goods.push(price);
+    buying_goods.push(revenue);
+    buying_log.push(buying_goods);
 
-calculate = {
-     numb1 : number1,
-     numb2 : number2,
-    result : null,
+    console.log(buying_goods);
+    console.log(buying_log);
+}
 
-        add : function  () {
-        result = number1 + number2;
-        console.log(result);
-    },
-        minus : function  () {
-        result = number1 - number2;
-        console.log(result);
-    },
-        mult : function  () {
-        result = number1 * number2;
-        console.log(result);
-    },
-        devide : function  () {
-        result = number1 / number2;
-        console.log(result);
-    },
-        display : function  () {
-        output.value = result;
-    }
-
-};
-
-
-add_oper.addEventListener('click', calculate.add );
-minus_oper.addEventListener('click', calculate.minus );
-mult_oper.addEventListener('click', calculate.mult );
-div_oper.addEventListener('click', calculate.devide );
-calculate_oper.addEventListener('click',  calculate.display );
+shop ('bread',  4, 10);*/
