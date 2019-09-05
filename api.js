a81226e5514565a6048a331c9057d56ee46747e3
vgybalo <vgybalo@ukr.net>
2019-09-05 22:06:19 +0300

    twenty seventh commit

--- api.js
+++ api.js
@@ -1,16 +1,23 @@
-    const fs = require('fs');
-    const path = require('path');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-//let filePath = path.join(__dirname,'file.js');
+async function getPic() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.work.ua/jobs-kyiv-it/?advs=1');
+  await page.waitFor(3000);
 
- exports.promiseReadfile = function (filePath) {
-    return new Promise((res, rej) => {       
-       fs.readFile(filePath, 'utf8', function(err, contents) {
-              res(contents);
-               rej(err);
-            });
-         })
+    for(let i=1; i<31; i++) {
+        let title = document.querySelector(`#pjax-job-list > div:nth-child(${i}) > h2 > a`);
+            fs.appendFile(path.join(__dirname,  'data.txt'), title, function(error){
+                if(error) throw error; 
+                
+                console.log(title);
+        });
     }
 
- 
-    
+   browser.close();
+  return result;
+}
+
+getPic();
--- readFile.js
+++
@@ -1,9 +0,0 @@
-const readFile  = require('./api.js');
-const path = require('path');
-
-let filePath = path.join(__dirname,'file.js');
-
-     readFile.promiseReadfile (filePath)
-            .then ( contents => console.log(contents))
-            .catch (err => console.error(err))
-
